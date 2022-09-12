1d51716b3bf3c378680fb097aeae597f2d416fa5
Mike Burns <13621631+burns238@users.noreply.github.com>
2022-09-12 14:47:31 +0100

    DDS2-496 - add hint text

--- test/views/notification/RelatesToViewSpec.scala
+++ test/views/notification/RelatesToViewSpec.scala
@@ -42,6 +42,46 @@ class RelatesToViewSpec extends ViewSpecBase with ViewMatchers {
       view.getElementsByClass("govuk-fieldset__heading").text() mustBe messages("relatesTo.heading")
     }
 
+    "have An individual as an option" in {
+      view.getElementsByClass("govuk-radios__label").get(0).text() mustBe messages("relatesTo.individual")
+    }
+    
+    "have hint text for individual option" in {
+      view.select("div#value_0-item-hint").text() must include(messages("relatesTo.hint.individual"))
+    }
+
+    "have An estate as an option" in {
+      view.getElementsByClass("govuk-radios__label").get(1).text() mustBe messages("relatesTo.estate")
+    }
+
+    "have hint text for estate option" in {
+      view.select("div#value_1-item-hint").text() must include(messages("relatesTo.hint.estate"))
+    }
+    
+    "have A company as an option" in {
+      view.getElementsByClass("govuk-radios__label").get(2).text() mustBe messages("relatesTo.company")
+    }
+
+    "have hint text for company option" in {
+      view.select("div#value_2-item-hint").text() must include(messages("relatesTo.hint.company"))
+    }
+    
+    "have A limited liability partnership as an option" in {
+      view.getElementsByClass("govuk-radios__label").get(3).text() mustBe messages("relatesTo.llp")
+    }
+
+    "have hint text for llp option" in {
+      view.select("div#value_3-item-hint").text() must include(messages("relatesTo.hint.llp"))
+    }
+
+    "have A trust as an option" in {
+      view.getElementsByClass("govuk-radios__label").get(4).text() mustBe messages("relatesTo.trust")
+    }
+
+    "have hint text for trust option" in {
+      view.select("div#value_4-item-hint").text() must include(messages("relatesTo.hint.trust"))
+    }
+
     "display the continue button" in {
       view.getElementsByClass("govuk-button").first() must haveId ("continue")
       view.getElementsByClass("govuk-button").text() mustBe messages("site.saveAndContinue")
