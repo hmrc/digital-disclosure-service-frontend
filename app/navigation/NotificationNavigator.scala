c5a06b91a287a5c320836edd8959e0c64447b915
Michele Satta <3033136+msatta@users.noreply.github.com>
2022-09-15 14:23:32 +0100

    Navigation from AreYouTheIndividual page to OffshoreLiabilities page

--- app/navigation/NotificationNavigator.scala
+++ app/navigation/NotificationNavigator.scala
@@ -27,13 +27,20 @@ import models._
 class NotificationNavigator @Inject()() {
 
   private val normalRoutes: Page => UserAnswers => Call = {
+
     case ReceivedALetterPage => ua => ua.get(ReceivedALetterPage) match { 
       case Some(true) => routes.LetterReferenceController.onPageLoad(NormalMode)
       case Some(false) => routes.RelatesToController.onPageLoad(NormalMode)
       case None => routes.ReceivedALetterController.onPageLoad(NormalMode)
     }
+
     case RelatesToPage => _ => routes.AreYouTheIndividualController.onPageLoad(NormalMode)
-  
+
+    case AreYouTheIndividualPage => ua => ua.get(AreYouTheIndividualPage) match {
+      case Some(_) => routes.OffshoreLiabilitiesController.onPageLoad(NormalMode)
+      case None => routes.AreYouTheIndividualController.onPageLoad(NormalMode)
+    }
+
     case _ => _ => controllers.routes.IndexController.onPageLoad
   }
 
--- test/navigation/NotificationNavigatorSpec.scala
+++ test/navigation/NotificationNavigatorSpec.scala
@@ -38,15 +38,29 @@ class NotificationNavigatorSpec extends SpecBase {
       }
 
       "must go from the ReceivedALetter page to the RelatesTo controller when the user selects No" in {
-        UserAnswers("id").set(ReceivedALetterPage, false) match { 
+        UserAnswers("id").set(ReceivedALetterPage, false) match {
           case Success(ua) => navigator.nextPage(ReceivedALetterPage, NormalMode, ua) mustBe routes.RelatesToController.onPageLoad(NormalMode)
           case Failure(e) => throw e
         }
       }
 
       "must go from the ReceivedALetter page to the LetterReference controller when the user selects Yes" in {
-        UserAnswers("id").set(ReceivedALetterPage, true) match { 
+        UserAnswers("id").set(ReceivedALetterPage, true) match {
           case Success(ua) => navigator.nextPage(ReceivedALetterPage, NormalMode, ua) mustBe routes.LetterReferenceController.onPageLoad(NormalMode)
+          case Failure(e) => throw e
+        }
+      }
+
+      "must go from the AreYouTheIndividual page to the OffshoreLiabilities controller when the user selects Yes" in {
+        UserAnswers("id").set(AreYouTheIndividualPage, AreYouTheIndividual.Yes) match {
+          case Success(ua) => navigator.nextPage(AreYouTheIndividualPage, NormalMode, ua) mustBe routes.OffshoreLiabilitiesController.onPageLoad(NormalMode)
+          case Failure(e) => throw e
+        }
+      }
+
+      "must go from the AreYouTheIndividual page to the OffshoreLiabilities controller when the user selects No" in {
+        UserAnswers("id").set(AreYouTheIndividualPage, AreYouTheIndividual.No) match {
+          case Success(ua) => navigator.nextPage(AreYouTheIndividualPage, NormalMode, ua) mustBe routes.OffshoreLiabilitiesController.onPageLoad(NormalMode)
           case Failure(e) => throw e
         }
       }
