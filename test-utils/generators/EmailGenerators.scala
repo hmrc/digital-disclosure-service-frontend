1200e059d04a71006a444e6eba23451db64755e7
Michele Satta <3033136+msatta@users.noreply.github.com>
2022-10-05 11:41:46 +0100

    DDS2-669 - Telephone Regex fixed, Test coverage improved, Refactoring of Telephone and Email generators

---
+++ test-utils/generators/EmailGenerators.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package generators
+
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalacheck.Gen.{alphaChar, alphaStr, chooseNum, listOfN}
+import uk.gov.hmrc.emailaddress.EmailAddress
+
+trait EmailGenerators {
+
+  val minUsernameLength = 3
+  val maxUsernameLength = 64
+
+  val minDomainLength = 5
+  val maxDomainLength = 253
+
+  val minPointDomainPosition = 2
+
+  def email(): Gen[String] = {
+    for {
+      usernameLength <- chooseNum(minUsernameLength, maxUsernameLength)
+      userName <- listOfN(usernameLength, alphaChar)
+      domainLength <- chooseNum(minDomainLength, maxDomainLength)
+      pointPosition <- chooseNum(minPointDomainPosition, domainLength - 1)
+      domain <- listOfN(pointPosition, alphaChar)
+      extension <- listOfN(domainLength - pointPosition, alphaChar)
+    } yield {
+      userName.mkString + "@" + domain.mkString + "." + extension.mkString
+    }
+  }
+
+  def invalidEmail(): Gen[String] = {
+    for {
+      str <- arbitrary[String] suchThat (_.nonEmpty)
+      if !EmailAddress.isValid(str)
+    } yield {
+      str
+    }
+  }
+
+  def invalidEmailDomain(): Gen[String] = {
+    for {
+      username <- alphaStr
+      domain <- alphaStr
+    } yield {
+      username + "@" + domain
+    }
+  }
+
+}
