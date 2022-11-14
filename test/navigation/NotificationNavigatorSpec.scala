9d89f9c1fc0e8851488aa33739c7866ae73df1fd
Michele Satta <3033136+msatta@users.noreply.github.com>
2022-11-14 17:02:26 +0000

    DDS2-1137: Navigation: What will the disclosure be about? (For estate)

--- test/navigation/NotificationNavigatorSpec.scala
+++ test/navigation/NotificationNavigatorSpec.scala
@@ -86,6 +86,13 @@ class NotificationNavigatorSpec extends SpecBase {
         }
       }
 
+      "must go from the RelatesTo page to the AreYouTheExecutorOfTheEstateController when select a Trust" in {
+        UserAnswers("id").set(RelatesToPage, RelatesTo.AnEstate) match {
+          case Success(ua) => navigator.nextPage(RelatesToPage, NormalMode, ua) mustBe routes.AreYouTheExecutorOfTheEstateController.onPageLoad(NormalMode)
+          case Failure(e) => throw e
+        }
+      }
+
       "must go from the AreYouTheIndividual page to the OffshoreLiabilities controller when the user selects Yes" in {
         UserAnswers("id").set(AreYouTheIndividualPage, true) match {
           case Success(ua) => navigator.nextPage(AreYouTheIndividualPage, NormalMode, ua) mustBe routes.OffshoreLiabilitiesController.onPageLoad(NormalMode)
