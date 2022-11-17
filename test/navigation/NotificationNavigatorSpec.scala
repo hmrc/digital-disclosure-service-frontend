5871b27543161697bf1fb0893bbfe7c5fa79a009
Michele Satta <3033136+msatta@users.noreply.github.com>
2022-11-17 12:08:41 +0000

    Navigation: Was the person registered for VAT, as far you're aware?(For estate)

--- test/navigation/NotificationNavigatorSpec.scala
+++ test/navigation/NotificationNavigatorSpec.scala
@@ -554,6 +554,28 @@ class NotificationNavigatorSpec extends SpecBase {
       "must go from the WhatWasThePersonNINOPage to the WasThePersonRegisteredForVATController when the user enter a valid NINO" in {
         navigator.nextPage(WhatWasThePersonNINOPage, NormalMode, UserAnswers("id")) mustBe routes.WasThePersonRegisteredForVATController.onPageLoad(NormalMode)
       }
+
+      "must go from the WasThePersonRegisteredForVATPage to the WhatWasThePersonVATRegistrationNumberController when the user selects Yes, and I know their VAT" in {
+        UserAnswers("id").set(WasThePersonRegisteredForVATPage, WasThePersonRegisteredForVAT.YesIKnow) match {
+          case Success(ua) => navigator.nextPage(WasThePersonRegisteredForVATPage, NormalMode, ua) mustBe routes.WhatWasThePersonVATRegistrationNumberController.onPageLoad(NormalMode)
+          case Failure(e) => throw e
+        }
+      }
+
+      "must go from the WasThePersonRegisteredForVATPage to the WasThePersonRegisteredForSAController when the user selects Yes, but I do not know their VAT" in {
+        UserAnswers("id").set(WasThePersonRegisteredForVATPage, WasThePersonRegisteredForVAT.YesButIDontKnow) match {
+          case Success(ua) => navigator.nextPage(WasThePersonRegisteredForVATPage, NormalMode, ua) mustBe routes.WasThePersonRegisteredForSAController.onPageLoad(NormalMode)
+          case Failure(e) => throw e
+        }
+      }
+
+      "must go from the IsTheIndividualRegisteredForSelfAssessmentPage to the WasThePersonRegisteredForSAController when the user selects No" in {
+        UserAnswers("id").set(WasThePersonRegisteredForVATPage, WasThePersonRegisteredForVAT.No) match {
+          case Success(ua) => navigator.nextPage(WasThePersonRegisteredForVATPage, NormalMode, ua) mustBe routes.WasThePersonRegisteredForSAController.onPageLoad(NormalMode)
+          case Failure(e) => throw e
+        }
+      }
+
     }
 
     "in Check mode" - {
