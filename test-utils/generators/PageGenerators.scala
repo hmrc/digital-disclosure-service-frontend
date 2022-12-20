490d260462dba84ba5e19ae53040b5495efdcf8c
chetan-jain-atos <112093269+chetan-jain-atos@users.noreply.github.com>
2022-12-20 11:38:17 +0000

    New Screen -You have left the service(Offshore liabilities)

--- test-utils/generators/PageGenerators.scala
+++ test-utils/generators/PageGenerators.scala
@@ -21,6 +21,9 @@ import pages._
 
 trait PageGenerators {
 
+  implicit lazy val arbitraryYouHaveLeftTheDDSPage: Arbitrary[YouHaveLeftTheDDSPage.type] =
+    Arbitrary(YouHaveLeftTheDDSPage)
+
   implicit lazy val arbitraryContractualDisclosureFacilityPage: Arbitrary[ContractualDisclosureFacilityPage.type] =
     Arbitrary(ContractualDisclosureFacilityPage)
 
--- test-utils/generators/UserAnswersEntryGenerators.scala
+++ test-utils/generators/UserAnswersEntryGenerators.scala
@@ -24,6 +24,14 @@ import play.api.libs.json.{JsValue, Json}
 
 trait UserAnswersEntryGenerators extends PageGenerators with ModelGenerators {
 
+  implicit lazy val arbitraryYouHaveLeftTheDDSUserAnswersEntry: Arbitrary[(YouHaveLeftTheDDSPage.type, JsValue)] =
+    Arbitrary {
+      for {
+        page  <- arbitrary[YouHaveLeftTheDDSPage.type]
+        value <- arbitrary[String].suchThat(_.nonEmpty).map(Json.toJson(_))
+      } yield (page, value)
+    }
+
   implicit lazy val arbitraryContractualDisclosureFacilityUserAnswersEntry: Arbitrary[(ContractualDisclosureFacilityPage.type, JsValue)] =
     Arbitrary {
       for {
