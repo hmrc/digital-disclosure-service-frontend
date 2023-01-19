580604cd9cc7d500e30683ae168f3635aee895ab
chetan-jain-atos <112093269+chetan-jain-atos@users.noreply.github.com>
2023-01-19 15:13:09 +0000

    Added navigation test cases

--- test/navigation/OtherLiabilitiesNavigatorSpec.scala
+++ test/navigation/OtherLiabilitiesNavigatorSpec.scala
@@ -21,6 +21,8 @@ import controllers.otherLiabilities.routes
 import pages._
 import models._
 
+import org.scalacheck.Arbitrary.arbitrary
+
 class OtherLiabilitiesNavigatorSpec extends SpecBase {
 
   val navigator = new OtherLiabilitiesNavigator
@@ -32,6 +34,128 @@ class OtherLiabilitiesNavigatorSpec extends SpecBase {
       "must go from a page that doesn't exist in the route map to Index" in {
         case object UnknownPage extends Page
         navigator.nextPage(UnknownPage, NormalMode, UserAnswers("id")) mustBe controllers.routes.IndexController.onPageLoad
+      }
+
+      "must go from the OtherLiabilityIssuesPage to the DescribeTheGiftController when InheritanceTaxIssues selected" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.InheritanceTaxIssues)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.AnIndividual)
+        } yield updatedUa
+
+        navigator.nextPage(OtherLiabilityIssuesPage, NormalMode, userAnswers.success.value) mustBe routes.DescribeTheGiftController.onPageLoad(NormalMode)
+      }
+
+      "must go from the OtherLiabilityIssuesPage to the WhatOtherLiabilityIssuesController when Other selected" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.Other)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.AnIndividual)
+        } yield updatedUa
+
+        navigator.nextPage(OtherLiabilityIssuesPage, NormalMode, userAnswers.success.value) mustBe routes.WhatOtherLiabilityIssuesController.onPageLoad(NormalMode)
+      }
+
+      "must go from the OtherLiabilityIssuesPage to the DidYouReceiveTaxCreditController when user an individual" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.VatIssues)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.AnIndividual)
+        } yield updatedUa
+
+        navigator.nextPage(OtherLiabilityIssuesPage, NormalMode, userAnswers.success.value) mustBe routes.DidYouReceiveTaxCreditController.onPageLoad(NormalMode)
+      }
+
+      "must go from the OtherLiabilityIssuesPage to the DidYouReceiveTaxCreditController when user an estate" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.VatIssues)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.AnEstate)
+        } yield updatedUa
+
+        navigator.nextPage(OtherLiabilityIssuesPage, NormalMode, userAnswers.success.value) mustBe routes.DidYouReceiveTaxCreditController.onPageLoad(NormalMode)
+      }
+
+      "must go from the OtherLiabilityIssuesPage to the CheckYourAnswersController when user is company/trust/llp" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.VatIssues)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.ACompany)
+        } yield updatedUa
+
+        navigator.nextPage(OtherLiabilityIssuesPage, NormalMode, userAnswers.success.value) mustBe routes.CheckYourAnswersController.onPageLoad
+      }
+
+      "must go from the DescribeTheGiftPage to the DidYouReceiveTaxCreditController when user an individual" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.VatIssues)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.AnIndividual)
+        } yield updatedUa
+
+        navigator.nextPage(DescribeTheGiftPage, NormalMode, userAnswers.success.value) mustBe routes.DidYouReceiveTaxCreditController.onPageLoad(NormalMode)
+      }
+
+      "must go from the DescribeTheGiftPage to the DidYouReceiveTaxCreditController when user an estate" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.VatIssues)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.AnEstate)
+        } yield updatedUa
+
+        navigator.nextPage(DescribeTheGiftPage, NormalMode, userAnswers.success.value) mustBe routes.DidYouReceiveTaxCreditController.onPageLoad(NormalMode)
+      }
+
+      "must go from the DescribeTheGiftPage to the WhatOtherLiabilityIssuesController when selected Other" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.Other)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.ACompany)
+        } yield updatedUa
+
+        navigator.nextPage(DescribeTheGiftPage, NormalMode, userAnswers.success.value) mustBe routes.WhatOtherLiabilityIssuesController.onPageLoad(NormalMode)
+      }
+
+      "must go from the DescribeTheGiftPage to the CheckYourAnswersController when user is company/trust/llp" in {
+        val answer: Set[OtherLiabilityIssues] = Set(OtherLiabilityIssues.VatIssues)
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(OtherLiabilityIssuesPage, answer)
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.ACompany)
+        } yield updatedUa
+
+        navigator.nextPage(DescribeTheGiftPage, NormalMode, userAnswers.success.value) mustBe routes.CheckYourAnswersController.onPageLoad
+      }
+
+      "must go from the WhatOtherLiabilityIssuesPage to the DidYouReceiveTaxCreditController when user an individual" in {
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(WhatOtherLiabilityIssuesPage, "answer")
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.AnIndividual)
+        } yield updatedUa
+
+        navigator.nextPage(WhatOtherLiabilityIssuesPage, NormalMode, userAnswers.success.value) mustBe routes.DidYouReceiveTaxCreditController.onPageLoad(NormalMode)
+      }
+
+      "must go from the WhatOtherLiabilityIssuesPage to the DidYouReceiveTaxCreditController when user an estate" in {
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(WhatOtherLiabilityIssuesPage, "answer")
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.AnEstate)
+        } yield updatedUa
+
+        navigator.nextPage(WhatOtherLiabilityIssuesPage, NormalMode, userAnswers.success.value) mustBe routes.DidYouReceiveTaxCreditController.onPageLoad(NormalMode)
+      }
+
+      "must go from the WhatOtherLiabilityIssuesPage to the CheckYourAnswersController when user is company/trust/llp" in {
+        val userAnswers = for {
+          ua          <- UserAnswers("id").set(WhatOtherLiabilityIssuesPage, "answer")
+          updatedUa 	<- ua.set(RelatesToPage, RelatesTo.ACompany)
+        } yield updatedUa
+
+        navigator.nextPage(WhatOtherLiabilityIssuesPage, NormalMode, userAnswers.success.value) mustBe routes.CheckYourAnswersController.onPageLoad
+      }
+
+      "must go from the DidYouReceiveTaxCreditPage to the CheckYourAnswersController when selected Yes/No" in {
+        val ua = UserAnswers("id").set(DidYouReceiveTaxCreditPage, arbitrary[Boolean].sample.value).success.value
+        navigator.nextPage(DidYouReceiveTaxCreditPage, NormalMode, ua) mustBe routes.CheckYourAnswersController.onPageLoad
       }
     }
 
