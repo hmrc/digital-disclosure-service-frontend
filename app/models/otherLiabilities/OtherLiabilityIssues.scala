befa7c621b2bad98e671fd6882b9b27a0425bb4c
Mike Burns <13621631+burns238@users.noreply.github.com>
2023-02-07 16:48:47 +0000

    DDS2-1626 - add new Other Liability issue and change order

--- app/models/otherLiabilities/OtherLiabilityIssues.scala
+++ app/models/otherLiabilities/OtherLiabilityIssues.scala
@@ -25,6 +25,7 @@ sealed trait OtherLiabilityIssues
 
 object OtherLiabilityIssues extends Enumerable.Implicits {
 
+  case object EmployerLiabilities extends WithName("employerLiabilities") with OtherLiabilityIssues 
   case object VatIssues extends WithName("vatIssues") with OtherLiabilityIssues
   case object InheritanceTaxIssues extends WithName("inheritanceTaxIssues") with OtherLiabilityIssues
   case object Class2National extends WithName("class2National") with OtherLiabilityIssues
@@ -32,9 +33,10 @@ object OtherLiabilityIssues extends Enumerable.Implicits {
   case object NoExclusion extends WithName("noExclusion") with OtherLiabilityIssues
 
   val values: Seq[OtherLiabilityIssues] = Seq(
-    VatIssues,
-    InheritanceTaxIssues,
+    EmployerLiabilities,
     Class2National,
+    InheritanceTaxIssues,
+    VatIssues,
     Other,
     NoExclusion
   )
