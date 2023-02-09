3fd42cc26138c55ed777c5fbe1aa2e0d5a0f8068
chetan-jain-atos <112093269+chetan-jain-atos@users.noreply.github.com>
2023-02-09 13:52:51 +0000

    fix

--- app/models/store/disclosure/OffshoreLiabilities.scala
+++ app/models/store/disclosure/OffshoreLiabilities.scala
@@ -37,10 +37,10 @@ final case class OffshoreLiabilities(
   maximumValueOfAssets: Option[TheMaximumValueOfAllAssets] = None
 ) {
   def isComplete = this match {
-    case OffshoreLiabilities(Some(_), _, _, _, Some(years),_ , _, Some(reasonableExcusePriorTo), _, _, _, _, _, _) if years == Set(ReasonableExcusePriorTo) => true
-    case OffshoreLiabilities(Some(_), _, _, _, Some(years),_ , _, _, Some(carelessPriorTo), _, _, _, _, _) if years == Set(CarelessPriorTo) => true
-    case OffshoreLiabilities(Some(_), _, _, _, Some(years),_ , _, _, Some(deliberatePriorTo), _, _, _, _, _) if years == Set(DeliberatePriorTo) => true
-    case OffshoreLiabilities(Some(_), _, _, _, Some(_),_ , _, _, _, Some(_), Some(_), _, _, Some(_)) => true
+    case OffshoreLiabilities(Some(_), _, _, _, Some(years), _, _, _, _, _, Some(reasonableExcusePriorTo), _, _, _, _) if years == Set(ReasonableExcusePriorTo) => true
+    case OffshoreLiabilities(Some(_), _, _, _, Some(years), _, _, _, _, _, Some(carelessPriorTo), _, _, _, _) if years == Set(CarelessPriorTo) => true
+    case OffshoreLiabilities(Some(_), _, _, _, Some(years), _, _, _, _, _, Some(deliberatePriorTo), _, _, _, _) if years == Set(DeliberatePriorTo) => true
+    case OffshoreLiabilities(Some(_), _, _, _, Some(years), _, _, _, _, _, Some(_), Some(_), _, _, Some(_)) => true
     case _ => false
   }
 }
