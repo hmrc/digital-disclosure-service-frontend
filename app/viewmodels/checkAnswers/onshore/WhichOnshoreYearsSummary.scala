ffca26ca67687d74670cbe7c54dccf85bff7f346
chetan-jain-atos <112093269+chetan-jain-atos@users.noreply.github.com>
2023-02-23 16:34:34 +0000

    CYA page for onshore

--- app/viewmodels/checkAnswers/onshore/WhichOnshoreYearsSummary.scala
+++ app/viewmodels/checkAnswers/onshore/WhichOnshoreYearsSummary.scala
@@ -17,16 +17,19 @@
 package viewmodels.checkAnswers
 
 import controllers.onshore.routes
-import models.{CheckMode, UserAnswers}
+import models.{CheckMode, UserAnswers, Behaviour, PriorToThreeYears, PriorToFiveYears, PriorToNineteenYears}
 import pages.WhichOnshoreYearsPage
 import play.api.i18n.Messages
 import play.twirl.api.HtmlFormat
 import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import services.OnshoreWhichYearsService
 import viewmodels.govuk.summarylist._
 import viewmodels.implicits._
+import com.google.inject.{Inject, Singleton}
 
-object WhichOnshoreYearsSummary  {
+@Singleton
+class WhichOnshoreYearsSummary @Inject() (onshoreWhichYearsService: OnshoreWhichYearsService)  {
 
   def row(answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
     answers.get(WhichOnshoreYearsPage).map {
@@ -35,7 +38,12 @@ object WhichOnshoreYearsSummary  {
         val value = ValueViewModel(
           HtmlContent(
             answers.map {
-              answer => HtmlFormat.escape(messages(s"whichOnshoreYears.$answer")).toString
+              answer => answer match {
+                case PriorToThreeYears => HtmlFormat.escape(messages(s"whichOnshoreYears.checkbox.any", onshoreWhichYearsService.getEarliestYearByBehaviour(Behaviour.ReasonableExcuse).toString)).toString
+                case PriorToFiveYears => HtmlFormat.escape(messages(s"whichOnshoreYears.checkbox.any", onshoreWhichYearsService.getEarliestYearByBehaviour(Behaviour.Careless).toString)).toString
+                case PriorToNineteenYears => HtmlFormat.escape(messages(s"whichOnshoreYears.checkbox.any", onshoreWhichYearsService.getEarliestYearByBehaviour(Behaviour.Deliberate).toString)).toString
+                case _ => HtmlFormat.escape(messages(s"whichOnshoreYears.checkbox", answer, (answer.toString.toInt+1).toString)).toString
+              }
             }
             .mkString(",<br>")
           )
