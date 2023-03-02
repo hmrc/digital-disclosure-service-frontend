859870092060bb2aacfbc761413cb9f631ff5a54
Michele Satta <3033136+msatta@users.noreply.github.com>
2023-03-02 15:41:03 +0000

    Test added

--- test/controllers/onshore/PropertyAddedControllerSpec.scala
+++ test/controllers/onshore/PropertyAddedControllerSpec.scala
@@ -1,18 +1,36 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import base.SpecBase
 import forms.PropertyAddedFormProvider
-import models.{NormalMode, UserAnswers}
+import models.address.{Address, Country}
+import models.{LettingProperty, NormalMode, UserAnswers}
 import navigation.{FakeOnshoreNavigator, OnshoreNavigator}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
-import pages.PropertyAddedPage
+import pages.{LettingPropertyPage, PropertyAddedPage}
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import services.SessionService
+import viewmodels.onshore.LettingPropertyModel
 import views.html.onshore.PropertyAddedView
 
 import scala.concurrent.Future
@@ -25,6 +43,9 @@ class PropertyAddedControllerSpec extends SpecBase with MockitoSugar {
   val form = formProvider()
 
   lazy val propertyAddedRoute = onshore.routes.PropertyAddedController.onPageLoad(NormalMode).url
+
+  val properties = Seq()
+  val index = 0
 
   "PropertyAdded Controller" - {
 
@@ -40,25 +61,7 @@ class PropertyAddedControllerSpec extends SpecBase with MockitoSugar {
         val view = application.injector.instanceOf[PropertyAddedView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form, NormalMode)(request, messages(application)).toString
-      }
-    }
-
-    "must populate the view correctly on a GET when the question has previously been answered" in {
-
-      val userAnswers = UserAnswers(userAnswersId).set(PropertyAddedPage, true).success.value
-
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
-
-      running(application) {
-        val request = FakeRequest(GET, propertyAddedRoute)
-
-        val view = application.injector.instanceOf[PropertyAddedView]
-
-        val result = route(application, request).value
-
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(form.fill(true), NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(form, properties, NormalMode)(request, messages(application)).toString
       }
     }
 
@@ -103,7 +106,7 @@ class PropertyAddedControllerSpec extends SpecBase with MockitoSugar {
         val result = route(application, request).value
 
         status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(boundForm, NormalMode)(request, messages(application)).toString
+        contentAsString(result) mustEqual view(boundForm, properties, NormalMode)(request, messages(application)).toString
       }
     }
 
@@ -136,5 +139,81 @@ class PropertyAddedControllerSpec extends SpecBase with MockitoSugar {
         redirectLocation(result).value mustEqual routes.IndexController.onPageLoad.url
       }
     }
+
+    "must redirect to the address of the first property page if remove method is called and there are no more properties" in {
+      val removePropertyRoute = onshore.routes.PropertyAddedController.remove(index, NormalMode).url
+
+      val mockSessionService = mock[SessionService]
+
+      when(mockSessionService.set(any())(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilderWithSessionService(userAnswers = Some(emptyUserAnswers), mockSessionService)
+          .overrides(
+            bind[OnshoreNavigator].toInstance(new FakeOnshoreNavigator(onwardRoute))
+          )
+          .build()
+
+      running(application) {
+        val request = FakeRequest(GET, removePropertyRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual letting.routes.RentalAddressLookupController.lookupAddress(index, NormalMode).url
+      }
+    }
+
+    "must redirect to the same page if remove method is called and there are still properties" in {
+      val removePropertyRoute = onshore.routes.PropertyAddedController.remove(index, NormalMode).url
+
+      val mockSessionService = mock[SessionService]
+
+      when(mockSessionService.set(any())(any())) thenReturn Future.successful(true)
+
+      val address: Address = Address(
+        line1 = "Line 1",
+        postcode = Some("AA112AA"),
+        line2 = None,
+        line3 = None,
+        line4 = None,
+        country = Country("AA")
+      )
+
+      val address2: Address = Address(
+        line1 = "Line 2",
+        postcode = Some("AA112AA"),
+        line2 = None,
+        line3 = None,
+        line4 = None,
+        country = Country("AA")
+      )
+
+      val property = LettingProperty(address = Some(address))
+      val property2 = LettingProperty(address = Some(address2))
+
+      val userAnswers = UserAnswers("id")
+        .addToSeq(LettingPropertyPage, property).success.value
+        .addToSeq(LettingPropertyPage, property2).success.value
+
+      val application =
+        applicationBuilderWithSessionService(userAnswers = Some(userAnswers), mockSessionService)
+          .overrides(
+            bind[OnshoreNavigator].toInstance(new FakeOnshoreNavigator(onwardRoute))
+          )
+          .build()
+
+      running(application) {
+        val request = FakeRequest(GET, removePropertyRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual onshore.routes.PropertyAddedController.onPageLoad(NormalMode).url
+      }
+    }
+
+
+
   }
 }
--- test/views/onshore/PropertyAddedViewSpec.scala
+++ test/views/onshore/PropertyAddedViewSpec.scala
@@ -17,33 +17,103 @@
 package views.onshore
 
 import base.ViewSpecBase
+import controllers.onshore.routes
 import forms.PropertyAddedFormProvider
+import models.address.{Address, Country}
 import play.twirl.api.Html
 import support.ViewMatchers
 import views.html.onshore.PropertyAddedView
-import models.NormalMode
+import models.{LettingProperty, NormalMode}
+import viewmodels.onshore.LettingPropertyModel
 
 class PropertyAddedViewSpec extends ViewSpecBase with ViewMatchers {
 
   val form = new PropertyAddedFormProvider()()
   val page: PropertyAddedView = inject[PropertyAddedView]
 
-  private def createView: Html = page(form, NormalMode)(request, messages)
+  val postcode1 = "AA111AA"
+  val address1: Address = Address(
+    line1 = "Line 1",
+    postcode = Some(postcode1),
+    line2 = None,
+    line3 = None,
+    line4 = None,
+    country = Country("AA")
+  )
+  val property1 = LettingProperty(address = Some(address1))
 
-  "view" should {
+  val postcode2 = "BB222BB"
+  val address2: Address = Address(
+    line1 = "Line 2",
+    postcode = Some(postcode2),
+    line2 = None,
+    line3 = None,
+    line4 = None,
+    country = Country("AA")
+  )
+  val property2 = LettingProperty(address = Some(address2))
 
+  "view for a single property" should {
+
+    val propertiesSummaries = LettingPropertyModel.row(Seq(property1), NormalMode)
+
+    def createView: Html = page(form, propertiesSummaries, NormalMode)(request, messages)
     val view = createView
 
     "have title" in {
-      view.select("title").text() must include(messages("propertyAdded.title"))
+      view.select("title").text() must include(messages("propertyAdded.title.single"))
     }
 
     "contain header" in {
-      view.getElementsByClass("govuk-fieldset__heading").text() mustBe messages("propertyAdded.heading")
+      view.getElementsByClass("govuk-heading-xl").text() mustBe messages("propertyAdded.heading.single")
+    }
+
+    "contain summary row" in {
+      view.getElementsByClass("govuk-summary-list__only_key").text() mustBe s"${address1.line1}, $postcode1"
+    }
+
+    "contain remove link" in {
+      view.getElementsByClass("summary-list-remove-link").first().text() must include(messages("site.remove"))
+      view.getElementsByClass("summary-list-remove-link").last().text() must include(messages("site.remove"))
     }
 
     "display the continue button" in {
       view.getElementsByClass("govuk-button").first() must haveId ("continue")
+      view.getElementsByClass("govuk-button").text() mustBe messages("site.saveAndContinue")
+    }
+
+  }
+
+  "view for a multiple properties" should {
+
+    val propertiesSummaries = LettingPropertyModel.row(Seq(property1, property2), NormalMode)
+
+    def createView: Html = page(form, propertiesSummaries, NormalMode)(request, messages)
+
+    val view = createView
+
+    "have title" in {
+      view.select("title").text() must include(messages("propertyAdded.title.multi", 2))
+    }
+
+    "contain header" in {
+      view.getElementsByClass("govuk-heading-xl").text() mustBe messages("propertyAdded.title.multi", 2)
+    }
+
+    "contain summary row" in {
+      view.getElementsByClass("govuk-summary-list__only_key").first().text() mustBe s"${address1.line1}, $postcode1"
+      view.getElementsByClass("govuk-summary-list__only_key").last().text() mustBe s"${address2.line1}, $postcode2"
+    }
+
+    "contain remove link" in {
+      view.getElementsByClass("summary-list-remove-link").first().text() must include(messages("site.remove"))
+      view.getElementsByClass("summary-list-remove-link").first().attr("href") mustBe routes.PropertyAddedController.remove(0, NormalMode).url
+      view.getElementsByClass("summary-list-remove-link").last().text() must include(messages("site.remove"))
+      view.getElementsByClass("summary-list-remove-link").last().attr("href") mustBe routes.PropertyAddedController.remove(1, NormalMode).url
+    }
+
+    "display the continue button" in {
+      view.getElementsByClass("govuk-button").first() must haveId("continue")
       view.getElementsByClass("govuk-button").text() mustBe messages("site.saveAndContinue")
     }
 
