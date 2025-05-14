589a0add4ec7957c8ad0816406cc037da8bddb31
AthishThayalan <120419238+AthishThayalan@users.noreply.github.com>
2025-05-14 11:18:32 +0100

    Remove unused arguments

--- app/controllers/auth/AuthController.scala
+++ app/controllers/auth/AuthController.scala
@@ -17,10 +17,8 @@
 package controllers.auth
 
 import config.FrontendAppConfig
-import controllers.actions.IdentifierAction
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import services.SessionService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 
 import javax.inject.Inject
@@ -28,9 +26,7 @@ import scala.concurrent.ExecutionContext
 
 class AuthController @Inject() (
   val controllerComponents: MessagesControllerComponents,
-  config: FrontendAppConfig,
-  sessionService: SessionService,
-  identify: IdentifierAction
+  config: FrontendAppConfig
 )(implicit ec: ExecutionContext)
     extends FrontendBaseController
     with I18nSupport {
