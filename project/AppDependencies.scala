59b63466bfbd5947f31dbcf1294b9cbd055674d4
AthishThayalan <120419238+AthishThayalan@users.noreply.github.com>
2025-06-13 14:15:26 +0100

    Upgrade dependencies to latest versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,17 +1,17 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "9.11.0"
+  val bootstrapVersion = "9.13.0"
   val mongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc"       %% "domain-play-30"                 % "10.0.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.0.0",
+    "uk.gov.hmrc"       %% "domain-play-30"                 % "11.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.6.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % mongoVersion,
     "org.typelevel"     %% "cats-core"                      % "2.8.0",
-    "uk.gov.hmrc"       %% "tax-year"                       % "5.0.0",
+    "uk.gov.hmrc"       %% "tax-year"                       % "6.0.0",
     "uk.gov.hmrc"       %% "auth-client-play-30"            % "8.6.0"
 
   )
