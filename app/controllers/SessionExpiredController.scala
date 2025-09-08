260e5716042080d70d42dd964a7d08de5a945977
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-09-08 15:20:38 +0100

    DDCYLS-6744 - GDS Timeout (#744)
    * Adding new session expired view and tests for the controller/view
    
    * updating app dependencies
    
    * correcting english and welsh messaging
    
    * changing auth on timeout view so it works in staging

--- app/controllers/SessionExpiredController.scala
+++ app/controllers/SessionExpiredController.scala
@@ -16,8 +16,6 @@
 
 package controllers
 
-import controllers.actions.IdentifierAction
-
 import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
@@ -25,13 +23,12 @@ import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.SessionExpiredView
 
 class SessionExpiredController @Inject() (
-  identify: IdentifierAction,
   val controllerComponents: MessagesControllerComponents,
   view: SessionExpiredView
 ) extends FrontendBaseController
     with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = identify { implicit request =>
+  def onPageLoad: Action[AnyContent] = Action { implicit request =>
     Ok(view()).withNewSession
   }
 }
