264493a64e057b4c5bb1c7c5b225a9bd372f187a
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2026-01-23 14:32:46 +0000

    DDCYLS-3549 - Showing all reasons on CYA page (#748)
    * removing the ellipsis overflow so that all reasons are displayed to the user
    
    * updating app dependencies
    
    * updating plugin dependency

--- app/viewmodels/checkAnswers/onshore/WhyAreYouMakingThisOnshoreDisclosureSummary.scala
+++ app/viewmodels/checkAnswers/onshore/WhyAreYouMakingThisOnshoreDisclosureSummary.scala
@@ -37,20 +37,18 @@ object WhyAreYouMakingThisOnshoreDisclosureSummary {
 
       val value = ValueViewModel(
         HtmlContent(
-          Text(
-            answers
-              .map { answer =>
-                HtmlFormat
-                  .escape(
-                    messages(
-                      if (areTheyTheIndividual) s"whyAreYouMakingThisDisclosure.you.$answer"
-                      else s"whyAreYouMakingThisDisclosure.$entity.$answer"
-                    )
+          answers
+            .map { answer =>
+              HtmlFormat
+                .escape(
+                  messages(
+                    if (areTheyTheIndividual) s"whyAreYouMakingThisDisclosure.you.$answer"
+                    else s"whyAreYouMakingThisDisclosure.$entity.$answer"
                   )
-                  .toString
-              }
-              .mkString("<br>")
-          ).withEllipsisOverflow(150).value
+                )
+                .toString
+            }
+            .mkString("<br>")
         )
       )
 
