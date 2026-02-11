b7f501a59b5f962e2ca50d0ed38afe6d95a9fbbc
AthishThayalan <120419238+AthishThayalan@users.noreply.github.com>
2026-02-11 11:58:51 +0000

    Remove comments

--- app/services/UAToDisclosureService.scala
+++ app/services/UAToDisclosureService.scala
@@ -76,7 +76,6 @@ class UAToDisclosureServiceImpl @Inject() (
     )
 
   def uaToOnshoreLiabilities(userAnswers: UserAnswers): Option[OnshoreLiabilities] = {
-    // Build behaviour from Page 2 selections, mapped to old enums
     val behaviour = buildBehaviourFromPage2(userAnswers)
 
     Some(
@@ -114,7 +113,6 @@ class UAToDisclosureServiceImpl @Inject() (
 
     val behaviours = scala.collection.mutable.Set[WhyAreYouMakingThisOnshoreDisclosure]()
 
-    // Map Page 2a selections to old enums
     userAnswers.get(WhyDidYouNotNotifyOnshorePage).foreach { selections =>
       selections.foreach {
         case DeliberatelyDidNotNotifyOnshore => behaviours += DeliberatelyDidNotNotify
@@ -123,7 +121,6 @@ class UAToDisclosureServiceImpl @Inject() (
       }
     }
 
-    // Map Page 2b selections to old enums
     userAnswers.get(WhyDidYouNotFileAReturnOnTimeOnshorePage).foreach { selections =>
       selections.foreach {
         case DeliberatelyWithheldInformation => behaviours += DeliberatelyDidNotFile
@@ -132,7 +129,6 @@ class UAToDisclosureServiceImpl @Inject() (
       }
     }
 
-    // Map Page 2c selections to old enums
     userAnswers.get(WhyYouSubmittedAnInaccurateOnshoreReturnPage).foreach { selections =>
       selections.foreach {
         case DeliberatelyInaccurate => behaviours += DeliberateInaccurateReturn
