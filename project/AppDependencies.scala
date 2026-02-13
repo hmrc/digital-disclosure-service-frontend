2a3ca0e3817b9d3bc117940b6a5975815c0fedcb
vjwatson <171040641+VJWatson@users.noreply.github.com>
2026-02-13 16:07:48 +0000

    failed CDIO, reverted domain play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc"       %% "domain-play-30"                 % "13.0.0",
+    "uk.gov.hmrc"       %% "domain-play-30"                 % "11.0.0",
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.31.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % mongoVersion,
     "org.typelevel"     %% "cats-core"                      % "2.8.0",
