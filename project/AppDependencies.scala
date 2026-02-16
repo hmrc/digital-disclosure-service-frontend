40505f1e1718150ef7fb16b4378fb7527b05a3e2
Vic Watson <171040641+VJWatson@users.noreply.github.com>
2026-02-16 09:07:07 +0000

    Ddcyls 8075 - app deps (#764)
    * app deps
    
    * failed CDIO, reverted domain play
    
    * app deps amended

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,12 +6,17 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion,
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
     "uk.gov.hmrc"       %% "domain-play-30"                 % "11.0.0",
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "12.31.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % mongoVersion,
     "org.typelevel"     %% "cats-core"                      % "2.8.0",
-    "uk.gov.hmrc"       %% "tax-year"                       % "6.0.0"
+    "uk.gov.hmrc"       %% "tax-year"                       % "6.0.0",
+    "uk.gov.hmrc"  %% s"play-frontend-hmrc-play-30"            % "12.31.0",
+    "org.apache.commons"   % "commons-lang3"               % "3.18.0",
+    "ch.qos.logback"       % "logback-core"                % "1.5.27",
+    "ch.qos.logback"       % "logback-classic"             % "1.5.27",
+    "at.yawk.lz4"          %  "lz4-java"                   % "1.10.3"
   )
 
   val test: Seq[ModuleID] = Seq(
