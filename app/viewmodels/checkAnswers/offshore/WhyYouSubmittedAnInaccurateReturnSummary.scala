769a03f2f58bf935b67b2569b1a81cc597139806
Bharti Verma <68339135+bharti5verma@users.noreply.github.com>
2026-02-19 09:21:27 +0000

    DDCYLS-8029 Onshore Offshore Liabilities summary pages updated (#768)

---
+++ app/viewmodels/checkAnswers/offshore/WhyYouSubmittedAnInaccurateReturnSummary.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.offshore
+
+import controllers.offshore.routes
+import models.{CheckMode, UserAnswers}
+import pages.WhyYouSubmittedAnInaccurateOffshoreReturnPage
+import play.api.i18n.Messages
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.govukfrontend.views.Aliases.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import viewmodels.govuk.all.FluentText
+import viewmodels.govuk.summarylist._
+import viewmodels.implicits._
+
+object WhyYouSubmittedAnInaccurateReturnSummary {
+
+  def row(userAnswers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
+    userAnswers.get(WhyYouSubmittedAnInaccurateOffshoreReturnPage).map { answers =>
+      val areTheyTheIndividual = userAnswers.isTheUserTheIndividual
+
+      val value = ValueViewModel(
+        HtmlContent(
+          Text(
+            answers
+              .map { answer =>
+                HtmlFormat
+                  .escape(
+                    messages(
+                      if (areTheyTheIndividual) s"WhyYouSubmittedAnInaccurateReturn.you.$answer"
+                      else s"WhyYouSubmittedAnInaccurateReturn.notYou.$answer"
+                    )
+                  )
+                  .toString
+              }
+              .mkString("<br>")
+          ).withEllipsisOverflow(150).value
+        )
+      )
+
+      SummaryListRowViewModel(
+        key = "WhyYouSubmittedAnInaccurateReturn.checkYourAnswersLabel",
+        value = value,
+        actions = Seq(
+          ActionItemViewModel("site.change", routes.WhyYouSubmittedAnInaccurateReturnController.onPageLoad(CheckMode).url)
+            .withVisuallyHiddenText(messages("WhyYouSubmittedAnInaccurateReturn.change.hidden"))
+        )
+      )
+    }
+
+}
