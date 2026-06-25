5f82576fd22570dd5d7d2f8afae03a2ae7217ad1
bhanu-nov1989 <177983246+bhanu-nov1989@users.noreply.github.com>
2026-06-25 13:51:34 +0100

    DDCYLS-7712 : Fix for VAT and SA data not saved.

--- app/controllers/notification/YourAddressLookupController.scala
+++ app/controllers/notification/YourAddressLookupController.scala
@@ -59,7 +59,7 @@ class YourAddressLookupController @Inject() (
           addressLookupService
             .getYourAddressLookupRedirect(continueUrl, request.userAnswers)
             .fold(
-              { e: Error =>
+              { (e: Error) =>
                 logger.error(s"Error initialising Address Lookup: $e")
                 Future.failed(e.throwable.getOrElse(new Exception(e.message)))
               },
