118e2641f2b34256ba2559b0d06b3352abda0b76
bharti.verma <68339135+bharti5verma@users.noreply.github.com>
2026-09-08 15:22:06 +0100

    DDCYLS-9025 Dependency updated

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
   val bootstrapVersion = "10.8.0"
-  val mongoVersion = "2.12.0"
+  val mongoVersion = "2.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
     "uk.gov.hmrc"       %% "domain-play-30"                 % "11.0.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "13.9.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "13.12.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % mongoVersion,
     "org.typelevel"     %% "cats-core"                      % "2.8.0",
     "uk.gov.hmrc"       %% "tax-year"                       % "6.0.0",
